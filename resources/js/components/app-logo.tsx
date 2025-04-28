104ec747c31749575f978b28e5317c2e84a8559b
macedera <michael.acedera@fasttrackph.com>
2025-04-28 17:38:19 +0800

    feat(): replace logo and text

--- resources/js/components/app-logo.tsx
+++ resources/js/components/app-logo.tsx
@@ -1,13 +1,13 @@
-import AppLogoIcon from './app-logo-icon';
+import { CheckCheckIcon } from 'lucide-react'
 
 export default function AppLogo() {
     return (
         <>
-            <div className="bg-sidebar-primary text-sidebar-primary-foreground flex aspect-square size-8 items-center justify-center rounded-md">
-                <AppLogoIcon className="size-5 fill-current text-white dark:text-black" />
+            <div className="bg-cyan-500 text-sidebar-primary-foreground flex aspect-square size-8 items-center justify-center rounded-md">
+                <CheckCheckIcon/>
             </div>
             <div className="ml-1 grid flex-1 text-left text-sm">
-                <span className="mb-0.5 truncate leading-none font-semibold">Laravel Starter Kit</span>
+                <span className="mb-0.5 truncate leading-none font-semibold">Project Task Tracker</span>
             </div>
         </>
     );
