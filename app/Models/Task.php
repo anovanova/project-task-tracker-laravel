4c24f12c7859026c6ab8d7bab2594369fadb65d3
macedera <michael.acedera@fasttrackph.com>
2025-04-28 22:36:46 +0800

    feat(): add task model

---
+++ app/Models/Task.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+
+class Task extends Model
+{
+    protected $fillable = [
+        'project_id',
+        'name',
+        'description',
+        'status',
+        'schedule_from',
+        'schedule_to',
+    ];
+}
