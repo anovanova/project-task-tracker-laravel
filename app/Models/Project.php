a4d7310b80900ce15b5b656654b6357d08c6cd06
macedera <michael.acedera@fasttrackph.com>
2025-04-28 22:36:37 +0800

    feat(): add project model

---
+++ app/Models/Project.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+
+class Project extends Model
+{
+    use HasUuids;
+
+    protected $fillable = [
+        'user_id',
+        'name',
+        'description',
+        'status',
+    ];
+
+    public function tasks(): HasMany
+    {
+        return $this->hasMany(Task::class);
+    }
+}
