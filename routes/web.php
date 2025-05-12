0fe7936118959efaba86caa5ce47063bfde9a4ad
macedera <michael.acedera@fasttrackph.com>
2025-05-12 15:23:20 +0800

    feat(): added routes

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\AuthenticatedSessionController;
 use App\Http\Controllers\AddProjectController;
 use App\Http\Controllers\ProjectController;
+use App\Http\Controllers\TaskController;
 use Inertia\Inertia;
 
 Route::get('/', [AuthenticatedSessionController::class, 'create'])->name('home');
@@ -19,6 +20,13 @@ Route::middleware(['auth', 'verified'])->group(function () {
 
     Route::get('get-projects', [ProjectController::class, 'index'])
     ->name('projects');
+
+    Route::get('project/{id}', [ProjectController::class, 'show'])
+    ->name('project');
+
+    Route::get('project/{id}/add-task', [TaskController::class, 'index'])->name('add-task-request');
+
+    Route::post('project/{id}/add-project-task', [TaskController::class, 'store'])->name('add-project-task');
 });
 
 require __DIR__.'/settings.php';
