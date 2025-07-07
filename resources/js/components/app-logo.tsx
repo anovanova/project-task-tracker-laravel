87b188d425be620e3d98d79bd1b996ed5fc6166a
macedera <michael.acedera@fasttrackph.com>
2025-07-07 21:30:17 +0800

    refactor(): change icon color

--- resources/js/components/app-logo.tsx
+++ resources/js/components/app-logo.tsx
@@ -1,10 +1,10 @@
-import { CheckCheckIcon } from 'lucide-react'
+import { CheckCheckIcon } from 'lucide-react';
 
 export default function AppLogo() {
     return (
         <>
-            <div className="bg-cyan-500 text-sidebar-primary-foreground flex aspect-square size-8 items-center justify-center rounded-md">
-                <CheckCheckIcon/>
+            <div className="text-sidebar-primary-foreground flex aspect-square size-8 items-center justify-center rounded-md bg-sky-500">
+                <CheckCheckIcon />
             </div>
             <div className="ml-1 grid flex-1 text-left text-sm">
                 <span className="mb-0.5 truncate leading-none font-semibold">Project Task Tracker</span>
